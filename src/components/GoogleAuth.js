f1528fb8c7a05c45a59ced766351fa666a3b8cda
Rohit Naik <rohit24naik@gmail.com>
2019-04-23 20:48:49 +0530

    another commit

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,35 +1,53 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
-    state= {isSignedIn:null};
+    state = { isSignedIn: null };
 
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
                 clientId: '520581988648-56m12n3lbvj476rjtn2hnm1b9ip2c0ok.apps.googleusercontent.com',
-                scope:'email'
-            }).then(()=>{
+                scope: 'email'
+            }).then(() => {
                 this.auth = window.gapi.auth2.getAuthInstance();
-                this.setState({isSignedIn:this.auth.isSignedIn.get()});
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() });
                 this.auth.isSignedIn.listen(this.onAuthChange)
             });
         });
     }
 
-    onAuthChange =() =>{
-      this.setState({isSignedIn:this.auth.isSignedIn.get()})
+    onAuthChange = () => {
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() })
     }
 
 
-    renderAuthButton(){
-        if(this.state.isSignedIn ===null){
-            return <div> Are we Signed IN ?</div>
-        }else{
-            if(this.state.isSignedIn){
-                return <div> We Are Signed IN ?</div>
+    onSignIn = () => {
+        this.auth.signIn();
+    }
+
+    onSignOut = () => {
+        this.auth.signOut();
+    }
+
+    renderAuthButton() {
+        if (this.state.isSignedIn === null) {
+            return null;
+        } else {
+            if (this.state.isSignedIn) {
+                return (
+                    <button onClick={this.onSignOut} className="ui red google button">
+                        <i className="google icon" />
+                        Sign Out
+                    </button>
+                )
             }
-            else{
-                return <div>We Are Not Signed IN ?</div>
+            else {
+                return (
+                    <button onClick={this.onSignIn} className="ui red google button">
+                        <i className="google icon" />
+                        Sign In
+                    </button>
+                )
             }
         }
     }
